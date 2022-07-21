cb6db20cdb6e9136d16bf43934a0e80b15fd54dd
Satya Deep Maheshwari <satyam@adobe.com>
2022-07-21 16:06:49 +0530

    feat: Added image table block

---
+++ blocks/image-table/image-table.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+    const imgDiv = document.createElement('div');
+    const itemsDiv = document.createElement('div');
+
+    block.querySelectorAll(':scope>div').forEach((row, idx) => {
+        if(idx === 0) {
+            const pic = row.querySelectorAll('picture')[0];
+            imgDiv.appendChild(pic);
+        } else {
+            const iconDiv = row.querySelectorAll(':scope>div')[0];
+            iconDiv.classList.add('icon-box');
+
+            const textDiv = row.querySelectorAll(':scope>div')[1];
+            textDiv.classList.add('text-box');
+            itemsDiv.appendChild(row);
+        }
+    });
+    block.innerHTML = '';
+    block.appendChild(imgDiv);
+    block.appendChild(itemsDiv);
+}
