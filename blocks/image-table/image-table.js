edea6ae7ced26bf70bd80dde53d105bb1b1fb9f3
Satya Deep Maheshwari <m.satyadeep@gmail.com>
2022-07-21 18:23:48 +0530

    feat: Added image table block (#26)
    * feat: Added image table block

---
+++ blocks/image-table/image-table.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+  const imgDiv = document.createElement('div');
+  const itemsDiv = document.createElement('div');
+
+  block.querySelectorAll(':scope>div').forEach((row, idx) => {
+    if (idx === 0) {
+      const pic = row.querySelectorAll('picture')[0];
+      imgDiv.appendChild(pic);
+    } else {
+      const classes = ['icon', 'text'];
+      row.querySelectorAll(':scope>div').forEach((item, j) => {
+        item.classList.add(`${classes[j]}-box`);
+      });
+      row.classList.add('image-table-row');
+      itemsDiv.appendChild(row);
+    }
+  });
+  block.innerHTML = '';
+  block.appendChild(imgDiv);
+  block.appendChild(itemsDiv);
+}
