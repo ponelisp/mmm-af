d0e2cb9ff6bc58c013e397242f40adf6c94a877b
Satya Deep Maheshwari <satyam@adobe.com>
2022-07-21 16:41:21 +0530

    chore: Fixed linter errors

--- blocks/image-table/image-table.js
+++ blocks/image-table/image-table.js
@@ -1,22 +1,22 @@
 export default function decorate(block) {
-    const imgDiv = document.createElement('div');
-    const itemsDiv = document.createElement('div');
+  const imgDiv = document.createElement('div');
+  const itemsDiv = document.createElement('div');
 
-    block.querySelectorAll(':scope>div').forEach((row, idx) => {
-        if(idx === 0) {
-            const pic = row.querySelectorAll('picture')[0];
-            imgDiv.appendChild(pic);
-        } else {
-            row.classList.add("image-table-row");
-            const iconDiv = row.querySelectorAll(':scope>div')[0];
-            iconDiv.classList.add('icon-box');
+  block.querySelectorAll(':scope>div').forEach((row, idx) => {
+    if (idx === 0) {
+      const pic = row.querySelectorAll('picture')[0];
+      imgDiv.appendChild(pic);
+    } else {
+      row.classList.add('image-table-row');
+      const iconDiv = row.querySelectorAll(':scope>div')[0];
+      iconDiv.classList.add('icon-box');
 
-            const textDiv = row.querySelectorAll(':scope>div')[1];
-            textDiv.classList.add('text-box');
-            itemsDiv.appendChild(row);
-        }
-    });
-    block.innerHTML = '';
-    block.appendChild(imgDiv);
-    block.appendChild(itemsDiv);
+      const textDiv = row.querySelectorAll(':scope>div')[1];
+      textDiv.classList.add('text-box');
+      itemsDiv.appendChild(row);
+    }
+  });
+  block.innerHTML = '';
+  block.appendChild(imgDiv);
+  block.appendChild(itemsDiv);
 }
